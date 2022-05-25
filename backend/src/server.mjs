755ce7ce3163a051bce97253991cbc20efbb9cc4
LauraJohnston20 <95102375+LauraJohnston20@users.noreply.github.com>
2022-05-24 21:48:30 -0230

    api/delete

--- backend/src/server.mjs
+++ backend/src/server.mjs
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
 
-app.get( '/hello', (req, res) => res.send('Hello there!'));
+app.delete( '/hello', (req, res) => res.send('Hello there!'));
 app.get( '/hello/:name', (req, res) => res.send(`Hello ${req.params.name}!`));
 app.post( '/hello', (req, res) => {
     console.log(JSON.stringify(req.body.name));
@@ -38,7 +38,29 @@ app.post('/api/addMovie', async (req, res) => {
 });
 
 // Delete movie
+app.delete('/api/delete/:id', async (req, res) => {
+    let id = req.params.id;
+    console.log("ID", id);
 
+    try {
+        await client.connect();
+        const db = client.db('movies');
+
+        const movieInfo = await db
+            .collection('mymovies')
+            .deleteOne({_id: new ObjectId(id)});
+        console.log(movieInfo);
+        if (movieInfo.deletedCount === 1) {
+            res.send("Delete successful.")
+        } else {
+            res.send("Delete unsuccessful.");
+        }
+    } catch (error) {
+        res.status(500);
+    } finally {
+        await client.close();
+    }
+});
 
 
 // Movie data
